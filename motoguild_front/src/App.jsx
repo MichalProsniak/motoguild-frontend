9b1ce31d6af3acc037d1ad3053021bbab0d88cf7
GrzegorzWikar <grzegorzwikar938@gmail.com>
2022-08-16 13:07:36 +0200

    Homepage

--- motoguild_front/src/App.jsx
+++ motoguild_front/src/App.jsx
@@ -1,13 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import Homepage from './components/Homepage'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <h1>HOME PAGE</h1>
+      <Homepage />
     </div>
   )
 }
