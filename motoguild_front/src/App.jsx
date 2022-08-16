70416e52227ba441645f23a67ed62e24a1626f01
Kamil Kornek <kamil.kornek96@gmail.com>
2022-08-16 12:02:17 +0200

    navbar

--- motoguild_front/src/App.jsx
+++ motoguild_front/src/App.jsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
+      <Navbar />
       <h1>HOME PAGE</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
