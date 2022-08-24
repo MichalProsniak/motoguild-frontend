71e49fdca8e97059df8890c0283f228b2e12943b
Michal Prosniak <michalprosniak@gmail.com>
2022-08-24 18:00:00 +0200

    button to participate

--- motoguild_front/src/components/RideBody.jsx
+++ motoguild_front/src/components/RideBody.jsx
@@ -17,31 +17,34 @@ export default function RideBody(props)
 
 
     return (
-        <div className="ride-for-list">
-            <Container>
-                <Row>
-                    <Col sm={6}>
-                        {isLoaded && <SmallMap small={false} originPoint={props.ride.startPlace} destinationPoint={props.ride.endingPlace} />}
-                    </Col>
-                    <Col sm={6}  >
-                        <h3>Opis przejazdu</h3>
-                        <p>{props.ride.description}</p>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col sm={6}  >
-                        <h4><i className="bi bi-person-circle"></i> {props.owner}OWNER NAME</h4><br></br>
-                        <ImportantRideInfo startPlace={props.ride.startPlace} endingPlace={props.ride.endingPlace} startTime={props.ride.startTime} />
-                        <Rating initialValue={props.ride.minimumRating} readonly={true} />
-                    </Col>
-                    <Col sm={6}>
-                        <br></br>
-                        <br></br>
-                        <br></br>
-                        <h1>POSTY</h1>
-                    </Col>
-                </Row>
-            </Container>
+        <div>
+            <div className="ride-for-list">
+                <Container>
+                    <Row>
+                        <Col sm={6}>
+                            {isLoaded && <SmallMap small={false} originPoint={props.ride.startPlace} destinationPoint={props.ride.endingPlace} />}
+                        </Col>
+                        <Col sm={6}  >
+                            <h3>Opis przejazdu</h3>
+                            <p>{props.ride.description}</p>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col sm={6}  >
+                            <h4><i className="bi bi-person-circle"></i> {props.owner}OWNER NAME</h4><br></br>
+                            <ImportantRideInfo startPlace={props.ride.startPlace} endingPlace={props.ride.endingPlace} startTime={props.ride.startTime} />
+                            <Rating initialValue={props.ride.minimumRating} readonly={true} />
+                        </Col>
+                        <Col sm={6}>
+                            <br></br>
+                            <br></br>
+                            <br></br>
+                            <h1>POSTY</h1>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+            <button>Weź udział</button>
         </div>
     )
 }
