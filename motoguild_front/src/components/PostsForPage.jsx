dbe6235f153b992152a036a6bbb7d8004127bd7a
Kamil Kornek <kamil.kornek96@gmail.com>
2022-09-08 17:42:21 +0200

    post for page component infinite api call fix

--- motoguild_front/src/components/PostsForPage.jsx
+++ motoguild_front/src/components/PostsForPage.jsx
@@ -1,42 +1,39 @@
 import Posts from "./Posts";
-import { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import { getPosts, createNewPost } from "../helpnigFunctions/ApiCaller";
 
-
-export default function PostsForPage(props)
-{
-  const loggedUser= {
+export default function PostsForPage(props) {
+  const loggedUser = {
     id: 2,
     userName: "Fineasz",
     email: "fin@gmail.com",
-    rating: 0
+    rating: 0,
+  };
+
+  const currentRide = useParams().id;
+  const [posts, setPosts] = useState([]);
+  const [postsLength, setPostsLength] = useState(0);
+
+  useEffect(() => {
+    const getPostsFromApi = async () => {
+      const postsFromServer = await getPosts(props.link, currentRide);
+      setPosts(postsFromServer);
+      setPostsLength(postsFromServer.length);
+    };
+    getPostsFromApi();
+  }, [postsLength]);
+
+  async function addPost(post) {
+    await createNewPost(props.link, currentRide, post);
+    const postsFromServer = await getPosts(props.link, currentRide);
+    setPosts(postsFromServer);
+    setPostsLength(postsFromServer.length);
   }
 
-    const currentRide = useParams().id;
-    const [posts,setPosts] = useState([])
-
-    useEffect(()=>{
-        const getPostsFromApi = async () => {
-          const postsFromServer = await getPosts(props.link, currentRide)
-          setPosts(postsFromServer)
-        }
-        getPostsFromApi()
-      },[posts])
-
-    async function addPost(post)
-    {
-      await createNewPost(props.link, currentRide, post)
-      setPosts([])
-    }
-
-    return (
-        <div className="posts" >
-            <Posts
-            loggedUser={loggedUser}
-            posts={posts}
-            onAdd={addPost}
-            />
-        </div>
-    )
+  return (
+    <div className="posts">
+      <Posts loggedUser={loggedUser} posts={posts} onAdd={addPost} />
+    </div>
+  );
 }
