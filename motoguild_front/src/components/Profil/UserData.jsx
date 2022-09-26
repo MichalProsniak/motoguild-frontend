0d5f54728fe168085747f20c1ddf53239ef519c1
GrzegorzWikar <grzegorzwikar938@gmail.com>
2022-09-26 11:50:02 +0200

    Week 5 Mondey

--- motoguild_front/src/components/Profil/UserData.jsx
+++ motoguild_front/src/components/Profil/UserData.jsx
@@ -1,10 +1,19 @@
+import { Rating } from "react-simple-star-rating";
+
 const UserData = ({user}) => {
+  console.log(user)
   return (
     <div className="user">
         <div className="user-data">
             <h3 className="user-data-element">Name: {user.userName}</h3>
             <h3 className="user-data-element">E-mail: {user.email}</h3>
             <h3 className="user-data-element">Phone Number: {user.phoneNumber}</h3>
+            <Rating
+              initialValue={user.rating}
+              readonly={true}
+              size={70}
+              className="ride-info-text-stars"
+            />
         </div>
     </div>
   )
