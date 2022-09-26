647ad24e4ef7358321782d38af5d41a343b6411b
Michal Prosniak <michalprosniak@gmail.com>
2022-09-26 14:03:11 +0200

    Group list and pagination styling

--- motoguild_front/src/pages/AllGroupsPage.jsx
+++ motoguild_front/src/pages/AllGroupsPage.jsx
@@ -4,7 +4,7 @@ export default function AllGroupsPage() {
   
   return (
     <div className="container-custom">
-      <h1>Grupy</h1>
+      <h1 className="groups-page-header" >Grupy</h1>
       <GroupList />
     </div>
   );
